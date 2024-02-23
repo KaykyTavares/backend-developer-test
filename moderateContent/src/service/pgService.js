3ba25aaf8d61868abdbd67f64f294300de9e75f2
Kayky Tavares Silva Alves <ktsalves@rd.com.br>
2024-02-23 00:25:25 -0300

    feat: create PostegreSQL service

---
+++ moderateContent/src/service/pgService.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg")
+const envVar = require("../config/envVars")
+   
+const pgClient = new Pool({
+    user: envVar.pgUser,
+    host: envVar.pgHost,
+    database: envVar.pgDB,
+    password: envVar.pgPassword,
+    port: envVar.pgPort
+})
+
+async function pgServiceQuery(query) {
+    try {
+        const queryRes = await pgClient.query(query)   
+
+        return queryRes
+    } catch (error) {
+        console.error(error)
+
+        return false
+    }
+}
+
+
+module.exports = {
+    pgServiceQuery
+}
